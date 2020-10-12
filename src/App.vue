ba0070292709197f9e029be4d41cd5736d6aba65
devhetu <developer.designer18@gmail.com>
2020-10-12 17:54:26 +0530

    Final Run condition map onchange map coordinates are changed also remove mark button added

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,21 @@
 <template>
   <div id="app" class="container">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-
-    <!-- <router-link to="components/map2">Map2</router-link> -->
-
     <div class="row">
-
       <!-- <Map/> -->
-      <!-- <Try/> -->
-      <Map2/>
+      
+      <Map2 />
     </div>
   </div>
 </template>
 <script>
 //import Map from "./components/Map.vue";
-// import Try from "./components/Try.vue"
-import Map2 from './components/Map2.vue'
+import Map2 from "./components/Map2.vue";
 export default {
   name: "App",
   components: {
     // Map,
-    // Try,
-    Map2    
+    Map2,
   },
 };
 </script>
